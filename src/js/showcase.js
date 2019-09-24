bae78c3d0a01d5c9fa294edd93c4ca1a8b85d0ec
EduardoGomezPueyo <tobacor9796@gmail.com>
2019-09-24 17:06:12 +0200

    Added slides, finished mobile design, addef favicon. Form utility not done yet

--- src/js/showcase.js
+++ src/js/showcase.js
@@ -1,40 +1,9 @@
-import {
-    $,
-    bindElement,
-} from './utils.js';
-
-//DOM elements
-const prevBtn = $(".prev");
-const nextBtn = $(".next");
-const dots = $(".dot-showcase");
-let slides = $(".showcase-slide");
-
-
-// Functions
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function showSlides(n) {
-    //Reset slides limits.
-    if (n > slides.length) {
-        slideIndex = 1
-    } else if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("active");
-        dots[i].classList.remove("active");
-    }
-    slides[slideIndex - 1].classList.add("active");
-    dots[slideIndex - 1].classList.add("active");
-}
-
-
-
-//Add functions
-bindElement(prevBtn, "click", () => showSlides(slideIndex += -1));
-bindElement(nextBtn, "click", () => showSlides(slideIndex += 1));
-
-dots.forEach(dot => {
-    bindElement(dot, "click", () => showSlides(slideIndex = parseInt(dot.getAttribute("name"), 10)))
-})
+new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
